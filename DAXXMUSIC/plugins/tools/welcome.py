f5b14f38d539ff2ffdf6cef85fcef28fe1b46498
synaxxlove <158541689+synaxxlove@users.noreply.github.com>
2024-02-04 16:28:34 +0530

    Update welcome.py

--- DAXXMUSIC/plugins/tools/welcome.py
+++ DAXXMUSIC/plugins/tools/welcome.py
@@ -46,13 +46,13 @@ def circle(pfp, size=(500, 500)):
 
 
 def welcomepic(pic, user, chatname, id, uname):
-    background = Image.open("AarohiX/assets/dil.png")
+    background = Image.open("DAXXMUSIC/assets/dil.png")
     pfp = Image.open(pic).convert("RGBA")
     pfp = circle(pfp)
     pfp = pfp.resize((1157, 1158))
     draw = ImageDraw.Draw(background)
-    font = ImageFont.truetype('AarohiX/assets/font.ttf', size=110)
-    welcome_font = ImageFont.truetype('AarohiX/assets/font.ttf', size=60)
+    font = ImageFont.truetype('DAXXMUSIC/assets/font.ttf', size=110)
+    welcome_font = ImageFont.truetype('DAXXMUSIC/assets/font.ttf', size=60)
     draw.text((1800, 700), f'NAME: {user}', fill=(255, 255, 255), font=font)
     draw.text((1800, 830), f'ID: {id}', fill=(255, 255, 255), font=font)
     draw.text((1800, 965), f"USERNAME : {uname}", fill=(255, 255, 255), font=font)
